d19f0149124d98e8ec192f873ac3a714ee1a04f3
Catriel Spalvieri <159962042+Argentino010@users.noreply.github.com>
2024-10-28 09:17:53 +0100

    Update index.py
    Nu har jag lagt while loop så att det går att fråga flera gånger istället för att bara fråga en gång.

--- Magic 8ball practice project-codecademy/index.py
+++ Magic 8ball practice project-codecademy/index.py
@@ -1,29 +1,37 @@
 import random
 
 namn = input("Vad heter du?")
-fråga = input("Fråga någonting ")
-svar = ""
 
-random_number = random.randint(1, 9)
+while True:
+    fråga = input("Fråga någonting ")
+    svar = ""
 
-if random_number == 1:
-    svar = "Ja, Naturligtvis"
-elif random_number == 2:
-    svar = "Självklart ja"
-elif random_number == 3:
-    svar = "Utan tvekan"
-elif random_number == 4:
-    svar = "Vet ej, fråga igen!"
-elif random_number == 5:
-    svar = "Fråga senare!"
-elif random_number == 6:
-    svar = "Ja, Jag tror det!"
-elif random_number == 7:
-    svar = "Jag tror inte det"
-elif random_number == 8:
-    svar = "Svårt att säga just nu"
-elif random_number == 9:
-    svar = "Nej!"
+    random_number = random.randint(1, 9)
 
-print(namn + " frågar: " + fråga)
-print("Magic 8-ball svarar: " + svar)
+    if random_number == 1:
+        svar = "Ja, Naturligtvis"
+    elif random_number == 2:
+        svar = "Självklart ja"
+    elif random_number == 3:
+        svar = "Utan tvekan"
+    elif random_number == 4:
+        svar = "Vet ej, fråga igen!"
+    elif random_number == 5:
+        svar = "Fråga senare!"
+    elif random_number == 6:
+        svar = "Ja, Jag tror det!"
+    elif random_number == 7:
+        svar = "Jag tror inte det"
+    elif random_number == 8:
+        svar = "Svårt att säga just nu"
+    elif random_number == 9:
+        svar = "Nej!"
+
+    print(namn + " frågar " + fråga)
+    print("Magic 8-ball svarar: " + svar)
+
+    avsluta = input("Vill du fråga något annat? (ja/nej): ")
+    if avsluta.lower() == "nej":
+        print("tack för att du frågar :)")
+        break
+
